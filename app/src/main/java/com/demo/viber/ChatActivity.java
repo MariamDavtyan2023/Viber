f054ba36e21f164647a175e325f46dc5450edc04
MariamDavtyan2023 <davtyan_mariam91@mail.ru>
2023-04-14 13:28:52 +0400

    Design ChatActivity

---
+++ app/src/main/java/com/demo/viber/ChatActivity.java
@@ -0,0 +1,36 @@
+package com.demo.viber;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewUser;
+    private View statusUser;
+    private RecyclerView recyclerViewMessages;
+    private EditText editTextMessage;
+    private ImageView imageViewSendMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        initViews();
+    }
+
+    private void initViews(){
+        textViewUser = findViewById(R.id.textViewUser);
+        statusUser = findViewById(R.id.statusUser);
+        recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+}
